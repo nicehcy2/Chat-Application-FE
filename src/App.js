cca60d558acac24112a79a526425a12b764c0715
heo <nicehcy2@naver.com>
2026-09-03 15:50:10 +0900

    [CHORE] CRA 템플릿 예제 파일 제거
    - App.css, logo.svg, App.test.js, setupTests.js, reportWebVitals.js 삭제
    - 어디서도 import되지 않던 src/index.css의 body 폰트 설정을 styles/index.css로 이동

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import './App.css';
 import Chat from './pages/Chat';
 import ChatRoomList from "./pages/ChatRoomList";
 import IphoneLayout from "./layouts/IphoneLayout";
